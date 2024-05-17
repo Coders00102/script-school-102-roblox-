4b478ee1067a6d45e4bc67673f3828042a62f4cf
Coders00102 <157604720+Coders00102@users.noreply.github.com>
2024-05-17 22:06:38 +0300

    Create script school 102 (roblox).lua

---
+++ script school 102 (roblox).lua
@@ -0,0 +1,43 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Osirius 1.0", "RJTheme1")
+
+-- game.Players.LocalPlayer.Character.Humanoid.MaxHealth = 5000
+
+local Tab = Window:NewTab("Speed")
+
+local Section = Tab:NewSection("Section Name")
+
+Section:NewSlider("Speed", "none", 500, 14, function(s) -- 500 (Макс.значение) | 0 (Мин.значение)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+
+end)
+
+Section:NewSlider("Jump", "SliderInfo", 500, 45, function(a)
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = a
+
+end)
+
+
+Section:NewSlider("Health (Patched)", "SliderInfo", 5000, 0, function()
+game.Players.LocalPlayer.Character.Humanoid.MaxHealth = 5000
+game.Players.LocalPlayer.Character.Humanoid.Health = b
+
+end)
+
+Section:NewTextBox("Height", "Рост игрока", function(txt)
+game.Players.LocalPlayer.Character.Humanoid.HipHeight = txt
+
+end)
+
+local Tab = Window:NewTab("Teleport")
+
+local Section = Tab:NewSection("Section Name")
+
+Section:NewButton("Teleport Vasya Bruklin", "ButtonInfo", function()
+Workspace.Live.ZERO201057.HumanoidRootPart.CFrame = Workspace.NPCPotato["peter griffin"].CFrame
+end)
+
+local Tab = Window:NewTab("Info")
+
+local Section = Tab:NewSection("Info")
